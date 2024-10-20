8c15f3ed39fe44784d95947b13079fd33b94c13d
MD. Ismail Hosen James <md.ismailhosenismailjames.com>
2024-10-20 19:54:13 +0600

    firebase config

---
+++ frontend_app/smart_home_automation/lib/src/auth/login/login_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
