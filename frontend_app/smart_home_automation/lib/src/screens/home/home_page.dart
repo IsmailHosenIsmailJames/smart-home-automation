f29f4238b418b36330094f9a25977b649b183774
MD. Ismail Hosen James <md.ismailhosenismailjames.com>
2024-11-02 18:06:52 +0600

    active of offline can be detected

--- frontend_app/smart_home_automation/lib/src/screens/home/home_page.dart
+++ frontend_app/smart_home_automation/lib/src/screens/home/home_page.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:firebase_database/firebase_database.dart';
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:smart_home_automation/src/theme/colors.dart';
@@ -15,155 +19,147 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        actions: [
+          StreamBuilder(
+            stream:
+                FirebaseDatabase.instance.ref().child('/last_active').onValue,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                String time = snapshot.data!.snapshot.value.toString();
+                final timeDataList = time.split("-");
+                int hour = (int.parse(timeDataList[3]) + 5) % 24;
+                int minute = int.parse(timeDataList[4]);
+                int second = int.parse(timeDataList[5]);
+                return StreamBuilder(
+                  stream: Stream.periodic(Duration(seconds: 1)),
+                  builder: (context, snapshot) {
+                    final now = DateTime.now();
+                    final lastUpdateTime = DateTime(
+                      now.year,
+                      now.month,
+                      now.day,
+                      hour,
+                      minute,
+                      second,
+                    );
+                    final difference =
+                        now.difference(lastUpdateTime).inSeconds.abs();
+                    log("Signal got $difference seconds earlier");
+                    return Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: difference > 30 ? Colors.red : Colors.green,
+                      ),
+                      padding: EdgeInsets.only(
+                        left: 10,
+                        right: 10,
+                        top: 2,
+                        bottom: 2,
+                      ),
+                      margin: EdgeInsets.only(
+                        right: 10,
+                      ),
+                      child: Text(
+                        difference > 30 ? "Offline" : "Active",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    );
+                  },
+                );
+              } else {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Icon(
+                      Icons.error_outline,
+                      color: Colors.red,
+                    ),
+                  );
+                }
+                return Center(
+                  child: SizedBox(
+                    height: 50,
+                    width: 50,
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              }
+            },
+          ),
+        ],
+      ),
       body: Center(
-        child: StreamBuilder<DatabaseEvent>(
-          stream: FirebaseDatabase.instance.ref().child('/elements').onValue,
-          builder:
-              (BuildContext context, AsyncSnapshot<DatabaseEvent> snapshot) {
-            if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else if (snapshot.hasData) {
-              Map<String, dynamic> data = Map<String, dynamic>.from(
-                  snapshot.data!.snapshot.value as Map);
-              List<ModeOfElements> elements = [];
-              List<String> elementsNames = [];
-              data.forEach(
-                (key, value) {
-                 if(key!= "names"){ elementsNames.add(key);}
-                if(key!= "names") {   elements.add(
-                    ModeOfElements.fromMap(Map<String, dynamic>.from(value)),
-                  );}
-                },
-              );
-              return SingleChildScrollView(
-                child: Column(
-                  children: List<Widget>.generate(
-                        elementsNames.length,
-                        (index) {
-                          return Container(
-                            padding: EdgeInsets.all(10),
-                            margin: EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              color: Colors.grey.shade300,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Row(
-                              children: [
-                                Text(
-                                  elementsNames[index],
-                                  style:
-                                      Theme.of(context).textTheme.titleMedium,
-                                ),
-                                Spacer(),
-                                if (elements[index].feedback != "done" &&
-                                    (elements[index].state !=
-                                        elements[index].request))
-                                  Stack(
-                                    alignment: Alignment.center,
-                                    children: [
-                                      CircularProgressIndicator(),
-                                      IconButton(
-                                        onPressed: () async {
-                                          toastification.show(
-                                            context: context,
-                                            type: ToastificationType.warning,
-                                            title: Text(
-                                                'The request has been Canceled'),
-                                            autoCloseDuration:
-                                                const Duration(seconds: 2),
-                                          );
-                                          await FirebaseDatabase.instance
-                                              .ref(
-                                                  "/elements/${elementsNames[index]}/request")
-                                              .set(elements[index].state);
-                                          await FirebaseDatabase.instance
-                                              .ref(
-                                                  "/elements/${elementsNames[index]}/feedback")
-                                              .set("done");
-                                        },
-                                        icon: Icon(
-                                          Icons.cancel_outlined,
-                                          color: Colors.red,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                Gap(10),
-                                SizedBox(
-                                  child: Switch(
-                                    value: elements[index].state,
-                                    onChanged: (value) async {
-                                      if (!(elements[index].feedback !=
-                                              "Done" &&
-                                          (elements[index].state !=
-                                              elements[index].request))) {
-                                        toastification.show(
-                                          context: context,
-                                          type: ToastificationType.info,
-                                          title: Text(
-                                              'Send a request to change the state of ${elementsNames[index]}'),
-                                          autoCloseDuration:
-                                              const Duration(seconds: 2),
-                                        );
-                                      } else {
-                                        toastification.show(
-                                          context: context,
-                                          type: ToastificationType.info,
-                                          title: Text(
-                                              'The request has already been sent'),
-                                          autoCloseDuration:
-                                              const Duration(seconds: 2),
-                                        );
-                                      }
-                                      await Future.delayed(
-                                          Duration(milliseconds: 300));
-                                      await FirebaseDatabase.instance
-                                          .ref(
-                                              "/elements/${elementsNames[index]}/request")
-                                          .set(!elements[index].state);
-                                      await FirebaseDatabase.instance
-                                          .ref(
-                                              "/elements/${elementsNames[index]}/feedback")
-                                          .set("Pending");
-                                    },
-                                  ),
-                                ),
-                                Gap(5),
-                                IconButton(
-                                  onPressed: () {},
-                                  icon: Icon(
-                                    Icons.edit,
-                                    color: AppColors().primaryColor,
-                                  ),
-                                ),
-                              ],
-                            ),
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.green.shade100,
+                ),
+                margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(20),
+                child: StreamBuilder(
+                  stream: FirebaseDatabase.instance.ref().child('/app').onValue,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      final data = snapshot.data!.snapshot.value!;
+                      List<String> dataList = List<String>.from(
+                        jsonDecode(
+                          jsonEncode(data),
+                        ),
+                      );
+                      return ListView.builder(
+                        itemCount: dataList.length,
+                        itemBuilder: (context, index) {
+                          List<String> infoList = dataList[index].split(":");
+                          return Row(
+                            children: [Text(infoList[2])],
                           );
                         },
-                      ) +
-                      <Widget>[
-                        Container(
-                          margin: EdgeInsets.all(10),
-                          width: double.infinity,
-                          child: TextButton.icon(
-                            style: TextButton.styleFrom(
-                              backgroundColor: Colors.grey.shade300,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                            ),
-                            onPressed: () {},
-                            icon: Icon(Icons.add),
-                            label: Text("Add New Element"),
-                          ),
-                        ),
-                      ],
+                      );
+                    } else {
+                      if (snapshot.hasError) {
+                        return Center(
+                          child: Text("Something went wrong"),
+                        );
+                      }
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  },
                 ),
-              );
-            } else {
-              return CircularProgressIndicator();
-            }
-          },
+              ),
+            ),
+            Expanded(
+              child: StreamBuilder(
+                stream: FirebaseDatabase.instance
+                    .ref()
+                    .child('/controller')
+                    .onValue,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(snapshot.data!.snapshot.value.toString());
+                  } else {
+                    if (snapshot.hasError) {
+                      return Center(
+                        child: Text("Something went wrong"),
+                      );
+                    }
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
